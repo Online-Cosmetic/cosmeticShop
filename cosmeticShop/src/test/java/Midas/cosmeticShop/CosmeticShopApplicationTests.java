e1d54bc347835c8bcd697eb9ab522ca965f57330
dragonwaterr <yongsu626@naver.com>
2025-04-06 00:21:37 +0900

    feat: Basic setting for Project

---
+++ cosmeticShop/src/test/java/Midas/cosmeticShop/CosmeticShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Midas.cosmeticShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
