48912ce41ec912ff9c15bf8ee1fd5a5afeb7bf98
izching <ggpr390917@gmail.com>
2025-10-14 17:32:43 +0900

    fix: vite.config.js 오류 수정
    fix: vite.config.js 오류 수정

--- cosmeticShop/src/main/frontend/cosmetic-shop/vite.config.js
+++ cosmeticShop/src/main/frontend/cosmetic-shop/vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr' // npm install vite-plugin-svgr --save-dev
 
