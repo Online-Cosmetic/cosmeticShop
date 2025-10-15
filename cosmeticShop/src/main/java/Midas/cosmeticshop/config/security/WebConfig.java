218c642037c35b2f39e57197512017667a5bb5d7
izching <ggpr390917@gmail.com>
2025-10-15 09:27:09 +0900

    fix: WebConfig 배포환경에서 동작하지 않도록 수정
    fix: WebConfig 배포환경에서 동작하지 않도록 수정

--- cosmeticShop/src/main/java/Midas/cosmeticshop/config/security/WebConfig.java
+++ cosmeticShop/src/main/java/Midas/cosmeticshop/config/security/WebConfig.java
@@ -1,6 +1,7 @@
 package Midas.cosmeticshop.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Path;  // ← 추가
 
 @Configuration
+@ConditionalOnProperty(name = "app.storage", havingValue = "local")
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${app.upload-dir}")
